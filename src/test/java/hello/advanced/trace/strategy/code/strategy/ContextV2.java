29716f0bcf741a4195b775bff7f5e77e38f52015
김준현 <sonjs01@naver.com>
2023-05-17 00:41:29 +0900

    불필요한 부분 제거 1. 주석삭제 2. unused Import 삭제

--- src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
+++ src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
@@ -1,14 +1,15 @@
-package hello.advanced.trace.strategy.code;
+package hello.advanced.trace.strategy.code.strategy;
 
 import lombok.extern.slf4j.Slf4j;
 
 /*
 * Context는 Strategy의 인터페이스만 의존하고 있다.
-* 덕분에 Strategy의 구현체를 변경하거나 새로 만들어도 Context 코드에는 영향을 주지 않는다.'
+* 덕분에 Strategy의 구현체를 변경하거나 새로 만들어도 Context 코드에는 영향을 주지 않는다.
 * 스프링에서 주로 사용하는 방식
 * */
 @Slf4j
 public class ContextV2 {
+    // execute가 호출될때마다 전달 받는다.
     public void execute(Strategy strategy){
         long startTime = System.currentTimeMillis();
         //비즈니르 로직 실행
